d4dfefa33b5c0287def0c8a8d6b6f6a5a3c0c765
Jintin <jintin.lin1018@gmail.com>
2018-04-08 23:01:38 +0800

    update layout

--- app/src/main/java/com/jintin/jutil/preference/JPreferenceActivity.java
+++ app/src/main/java/com/jintin/jutil/preference/JPreferenceActivity.java
@@ -3,7 +3,6 @@ package com.jintin.jutil.preference;
 import android.os.Bundle;
 import android.support.annotation.Keep;
 
-import com.jintin.jutil.R;
 import com.jintin.jutil.app.JActivity;
 
 
@@ -17,9 +16,8 @@ public abstract class JPreferenceActivity extends JActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preference);
         JPreferenceFragment fragment = JPreferenceFragment.getInstance(getPreferenceView(), getFacebookId(), getFacebookUrl());
-        getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+        getFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
     }
 
     public abstract int getPreferenceView();
